761ca7a755dc05dfa27e72f339f06c05f65f8e5b
Lekheshwar <hilekhesh20@gmail.com>
2021-06-28 14:23:09 +0530

    Medium Tricky

---
+++ Day-11/SingleElementInaSortedArray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int lo = 0, hi = nums.size() - 2;
+        while(lo <= hi){
+            int mid = (hi + lo) >> 1;
+            if(nums[mid] == nums[mid ^ 1])
+                lo = mid + 1;
+            else
+                hi = mid - 1;
+        }
+        return nums[lo];
+    }
+};
